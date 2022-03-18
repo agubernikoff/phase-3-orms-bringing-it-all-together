adfc30f724bf486d9ca396b785ab9ccd48cd48b3
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-17 21:57:37 -0400

    commit tree 962a5af67b6906ed9c2a4bd751e14b837e7955fe

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,7 +28,7 @@ class Dog
         DB[:conn].execute(sql,self.name,self.breed)
 
         self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-
+        binding.pry
         self
     end
 
